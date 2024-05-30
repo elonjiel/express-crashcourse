25dc677ed4f8a6f6d62e6b75054e97464f631359
Elon Jiel <elonudemy@gmail.com>
2024-05-30 19:58:05 +0800

    Added Core Modules

--- server.js
+++ server.js
@@ -1,5 +1,67 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+import posts from "./routes/posts.js"
+
+// const express = require("express");
+// const path = require("path");
+// const posts = require("./routes/posts");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// Routes
+
+app.use("/api/posts", posts);
+
+// setup static folder
+// app.use(express.static(path.join(__dirname, "public")));
+
+// let posts = [
+//     {id: 1, title: "Post One"},
+//     {id: 2, title: "Post Two"},
+//     {id: 3, title: "Post Three"},
+    
+// ];
+// // Get All posts
+// app.get('/api/posts', (req, res) => {
+//     const limit = parseInt(req.query.limit);
+
+//     if (!isNaN(limit) && limit > 0) {
+//        return res.status(200).json(posts.slice(0, limit));
+//     } 
+       
+//     res.status(200).json(posts);
+    
+// });
+
+// // Get single post
+// app.get('/api/posts/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     const post = posts.find((post) => post.id === id);
+
+//     if (!post) {
+//        return res
+//        .status(404)
+//        .json({message: `A post with the id of ${id} was not found`});
+//     } 
+          
+//     res.status(200).json(post);
+    
+// });
+
+
+
+//** res.sendFile method **
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+// })
+// app.get("/about", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "about.html"));
+// })
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
